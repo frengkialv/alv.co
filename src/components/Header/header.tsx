02e7492e8621be1b40301658d1da4e6f02d8ab48
unknown <and.frengky@gmail.com>
2024-08-20 03:59:28 +0700

    change spacing

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -33,6 +33,7 @@ const Wrapper = styled.header<{ $superHeader: boolean }>`
 
   @media ${QUERIES.phoneAndSmaller} {
     --top: ${(props) => (props.$superHeader ? -42 + "px" : 0)};
+
     top: var(--top);
   }
 `;
